d1617782c076f93f81f75e32b5e0a6af29afb26f
Trevor <trevor@pinkbike.com>
2019-10-16 13:20:35 -0700

    temp progress of locationPermissionCheck for Ben

--- www/CheckGPS.js
+++ www/CheckGPS.js
@@ -1,5 +1,12 @@
-exports.check=function(success, error, options){
-		options = options || {};
-		options.alert = false || options.alert;
-		cordova.exec(success, error, "CheckGPS", "check", [options.alert]);
+var PluginFunctions = {
+    check: function(success, error, options) {
+        options = options || {};
+        options.alert = false || options.alert;
+        cordova.exec(success, error, "CheckGPS", "check", [options.alert]);
+    },
+    locationPermissionCheck: function(cb) {
+        cordova.exec(cb, null, "CheckGPS", 'locationPermissionCheck', []);
+    },
 };
+
+exports = PluginFunctions;
