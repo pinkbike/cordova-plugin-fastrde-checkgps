9e65a4d709aa2fb69a86b05a2f0b2e23c51b9373
Benjamin Thomas <ben@bentomas.com>
2020-02-20 23:07:28 -0800

    fill in ios functionality

--- src/ios/CDVCheckGPS.m
+++ src/ios/CDVCheckGPS.m
@@ -1,12 +1,10 @@
 #import "CDVCheckGPS.h"
 #import <CoreLocation/CoreLocation.h>
-
 @implementation CheckGPS
-
 - (void)check:(CDVInvokedUrlCommand*)command
 {
-	CDVPluginResult* pluginResult = nil;
-	if([CLLocationManager locationServicesEnabled] && 
+    CDVPluginResult* pluginResult = nil;
+	if([CLLocationManager locationServicesEnabled] &&
   	 [CLLocationManager authorizationStatus] != kCLAuthorizationStatusDenied) {
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 	} else {
@@ -14,19 +12,30 @@
 	}
   [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
 - (void)locationServiceCheck:(CDVInvokedUrlCommand*)command
 {
-	check:(CDVInvokedUrlCommand*)command;
+    CDVPluginResult* pluginResult = nil;
+    if ([CLLocationManager locationServicesEnabled]) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
 - (void)locationPermissionCheck:(CDVInvokedUrlCommand*)command
 {
-	check:(CDVInvokedUrlCommand*)command;
+    CDVPluginResult* pluginResult = nil;
+    if ([CLLocationManager locationServicesEnabled] && [CLLocationManager authorizationStatus] != kCLAuthorizationStatusDenied) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
 - (void)locationPermissionAsk:(CDVInvokedUrlCommand*)command
 {
-	check:(CDVInvokedUrlCommand*)command;
+    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+    [locationManager requestWhenInUseAuthorization];
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
 }
 @end
